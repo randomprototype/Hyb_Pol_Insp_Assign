84bf2d396d9ed4b2eb0c09ef42ac6e77a3843492
randomprototype <100228373+randomprototype@users.noreply.github.com>
2024-08-30 14:39:03 -0300

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -60,8 +60,14 @@ def main():
         Cr=st.number_input("Insert cost of replacement (inspections and age-based) (C\u02b3)", min_value = 0.5, value = 1.0)
         Cf=st.number_input("Insert cost of failure (C\u1da0)", min_value = 5, value = 10)
         
-        st.button("Add Repairperson", on_click=add_column, help="You should add columns to the parameters of the repairperson as you want (each column is related to one repairperson).")
-        st.button("Remove Column", on_click=remove_column)
+        col1, col2 = st.columns(2)
+
+        # Botões com balões informativos, cada um em uma coluna
+        with col1:
+            st.button("Add Repairperson", on_click=add_column, help="You should add columns to the parameters of the repairperson as you want (each column is related to one repairperson).")
+        
+        with col2:
+            st.button("Remove Repairperson", on_click=remove_column)
         
         columns = st.columns(st.session_state.num_columns)
 
