ec7434a0229b15917d3a5de3e1657adbb06f7052
randomprototype <100228373+randomprototype@users.noreply.github.com>
2024-08-30 14:31:17 -0300

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -46,9 +46,6 @@ def main():
         def add_column():
             st.session_state.num_columns += 1
         
-        # Botão para adicionar uma coluna
-        st.button("Add Column", on_click=add_column)
-        
         st.subheader("Insert the parameter values below:")
         
         Eta1=st.number_input("Insert the characteristic life of the weak component (η\u2081)", min_value = 0.0, value = 3.0)
@@ -59,6 +56,8 @@ def main():
         Lambda=st.number_input("Insert the rate of the exponential distribution for delay-time (λ)", min_value = 0.0, value = 2.0)
         Cr=st.number_input("Insert cost of replacement (inspections and age-based) (C\u02b3)", min_value = 0.5, value = 1.0)
         Cf=st.number_input("Insert cost of failure (C\u1da0)", min_value = 5, value = 10)
+        
+        st.button("Add Column", on_click=add_column)
         columns = st.columns(st.session_state.num_columns)
 
         columns[0].text_input("Insert the fixed costs of the inspectors (C\u02b0)", value="0.1 0.1")
