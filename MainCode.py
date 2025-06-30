f1bf2bb911c592340969e1cc64e83a0bbea63193
randomprototype <100228373+randomprototype@users.noreply.github.com>
2025-06-30 11:41:24 -0300

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -84,7 +84,7 @@ def main():
         Y=[-1]
         st.subheader("Insert the variable values below:")
         K=int(st.text_input("Insert the number of inspections (K)", value=4))
-        delta=st.text_input("Insert the inspection moments (Δ), following the example below",value="2,00 4,00 8,00 10,00", key=f"Delta")
+        delta=st.text_input("Insert the inspection moments (Δ), following the example below",value="2.00 4.00 8.00 10.00", key=f"Delta")
         Delta=[float(x.replace(",", ".")) for x in delta.split()]
         for i, col in enumerate(st.columns(K)):
             col.write(f"**{i+1}-th inspection:**")
