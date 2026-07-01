5df8eb9ad68b3e84b23822705181e9f1aeee74db
randomprototype <100228373+randomprototype@users.noreply.github.com>
2026-07-01 15:03:45 -0300

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -10,8 +10,6 @@ click==8
 """
 import streamlit as st
 import numpy as np
-import sys
-from streamlit import cli as stcli
 from scipy.integrate import quad #Single integral
 from scipy.integrate import dblquad
 from PIL import Image
@@ -366,8 +364,5 @@ c.a.v.cavalcante@random.org.br
                  in order to bring together different researchers who work in the following areas: risk, maintenance a
                  nd operation modelling. Learn more about it through our website.''')
         st.markdown('[Click here to be redirected to our website](https://sites.ufpe.br/random/#page-top)',False)        
-if st._is_running_with_streamlit:
+if __name__ == "__main__":
     main()
-else:
-    sys.argv = ["streamlit", "run", sys.argv[0]]
-    sys.exit(stcli.main())
